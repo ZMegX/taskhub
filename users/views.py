2014a75e356b000b262dc033088a06ec95f9b225
ZMegX <picardomegan@gmail.com>
2025-07-27 17:02:59 +0100

    customUserCreationForm added + route to dashboard

--- users/views.py
+++ users/views.py
@@ -2,20 +2,22 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required #Added import here
 from django.contrib import messages #import for messages
 
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm 
+from .forms import UserUpdateForm, ProfileUpdateForm, CustomUserCreationForm
 
 # Create your views here.
-# Create your views here.
+def dashboard(request):
+    return render(request, "users/dashboard.html")
+
 def register(request):
   if request.method == 'POST':
-    form = UserRegisterForm(request.POST)
+    form = CustomUserCreationForm(request.POST)
     if form.is_valid():
       form.save()
       username = form.cleaned_data.get('username')
       messages.success(request, f'Account created for {username}!')
       return redirect('login')
   else:
-    form = UserRegisterForm()
+    form = CustomUserCreationForm(request.POST)
   return render(request, 'registration/sign_up.html', {'form': form})
 
 @login_required
@@ -39,4 +41,4 @@ def profile(request):
     'p_form': p_form
   }
 
-  return render(request, 'templates/profile.html', context)
+  return render(request, 'users/profile.html', context)
