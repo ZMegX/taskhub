a41a05be649d7572d724d2dec770d4137b3f83dc
ZMegX <picardomegan@gmail.com>
2025-07-27 16:59:26 +0100

    acc dashboard path added

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,7 +8,9 @@ from . import views
 from users import views as user_views
 
 urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("dashboard/", views.dashboard, name="dashboard"),
     path('profile/', user_views.profile, name='profile'),    
     path("register/", views.register, name="sign_up"),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
