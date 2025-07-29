fc2447daec5b78bf858dde25aeaf3e271afed9a5
ZMegX <picardomegan@gmail.com>
2025-07-29 19:48:40 +0100

    renamed to users

---
+++ users/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required #Added import here
+from django.contrib import messages #import for messages
+
+from .forms import UserUpdateForm, ProfileUpdateForm, CustomUserCreationForm
+
+# Create your views here.
+def dashboard(request):
+    return render(request, "users/dashboard.html")
+
+def register(request):
+  if request.method == 'POST':
+    form = CustomUserCreationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      username = form.cleaned_data.get('username')
+      messages.success(request, f'Account created for {username}!')
+      return redirect('login')
+  else:
+    form = CustomUserCreationForm(request.POST)
+  return render(request, 'registration/sign_up.html', {'form': form})
+
+@login_required
+def profile(request):
+  if request.method == 'POST':
+    u_form = UserUpdateForm(request.POST, instance=request.user)
+    p_form = ProfileUpdateForm(request.POST, 
+                request.FILES, 
+                instance=request.user.profile)
+    if u_form.is_valid() and p_form.is_valid():
+      u_form.save()
+      p_form.save()
+      messages.success(request, f'Your account has been updated') #Changes here
+      return redirect('profile') #Changes here
+  else:
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
+
+  context = {
+    'u_form': u_form,
+    'p_form': p_form
+  }
+
+  return render(request, 'users/profile.html', context)
